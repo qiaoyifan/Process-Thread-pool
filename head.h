8ac364da891065d10bff1a145b8466272a7b796e
qiaoyifan <qyf116126@163.com>
2017-09-24 22:42:55 +0800

    first

---
+++ head.h
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: head.h
+	> Author:qiaoyifan 
+	> Mail: qyf116126@163.com
+	> Created Time: 2017年09月12日 星期二 10时56分24秒
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+
+#endif
